0aecce84b95ef2626b8c0ab5f880b36f9c4dafcc
agusberrutti <agustina.berrutti@correo.ucu.edu.uy>
2025-09-06 14:35:35 -0300

    Board

---
+++ src/Program/Board.cs
@@ -0,0 +1,39 @@
+/*
+ Esta clase lo que hace es representar directamente el tablero, contando el largo y ancho del tablero y si las células 
+ están vivas o muertas, guardando estos valores únicamente para usarlos después en otras partes del programa.
+*/
+using System.Diagnostics;
+
+namespace Ucu.Poo.GameOfLife;
+
+public class Board //hace pública esta clase para que pueda ser utilizada luego, fuera de este código
+{
+    private bool[,] _Cells;
+    public bool [,] Cells // representa al tablero con dos valores, true (célula viva) y false (célula muerta)
+    {
+        get { return _Cells; }
+        set { _Cells = value; }
+    }
+
+    private int _Width;
+    public int Width //guarda el ancho del tablero
+    {
+        get { return _Width;}
+        set { _Width = value; }
+    }
+
+    private int _Height;
+
+    public int Height //guarda el largo del tablero
+    {
+        get { return _Height;}
+        set { _Height = value; }
+    }
+
+    public Board(bool[,] cells) //construye o inicializa el tablero con los datos que le llegan (las cells (bool) que le llega), calculando sus dimensiones
+    {
+        Cells = cells;
+        Width = cells.GetLength(0);
+        Height = cells.GetLength(1);
+    }
+}
