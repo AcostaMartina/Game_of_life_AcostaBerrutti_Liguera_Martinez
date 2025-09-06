6079dff52b7fd2e6057e377122e36cda5405211c
FrancoLiguera2 <franco.liguera@correo.ucu.edu.uy>
2025-09-06 16:33:56 -0300

    FUNCIONA

--- src/Program/GameLogic.cs
+++ src/Program/GameLogic.cs
@@ -7,7 +7,7 @@ namespace Ucu.Poo.GameOfLife;
 //y no es experta ni en almacenamiento de datos, visualización ni lectura de archivos. 
 public class GameLogic
 {
-    private Board _board;
+    public Board _board;
 
     public GameLogic(Board board)
     {
@@ -22,7 +22,7 @@ public class GameLogic
         
         //Crea un nuevo tablero para la nueva generación sin modificar el anterior.
         bool[,] newCells = new bool[boardWidth, boardHeight];
-        Board cloneboard = new Board(newCells);
+        bool[,] cloneboard = new bool[boardWidth, boardHeight];
         
         //Recorre todas las celulas del tablero actual
         for (int x = 0; x < boardWidth; x++)
@@ -52,26 +52,32 @@ public class GameLogic
                 if (_board.Cells[x, y] && aliveNeighbors < 2)
                 {
                     //Celula muere por baja población
-                    cloneboard.Cells[x, y] = false;
+                    cloneboard[x, y] = false;
                 }
                 else if (_board.Cells[x, y] && aliveNeighbors > 3)
                 {
                     //Celula muere por sobrepoblación
-                    cloneboard.Cells[x, y] = false;
+                    cloneboard[x, y] = false;
                 }
                 else if (!_board.Cells[x, y] && aliveNeighbors == 3)
                 {
                     //Celula nace por reproducción
-                    cloneboard.Cells[x, y] = true;
+                    cloneboard[x, y] = true;
                 }
                 else
                 {
                     //Celula mantiene el estado que tenía
-                    cloneboard.Cells[x, y] = _board.Cells[x, y];
+                    cloneboard[x, y] = _board.Cells[x, y];
                 }
             }
         }
-        //Reemplaza el anterior tablero con el nuevo.
-        _board = cloneboard;
+        //Actualiza el tablero original.
+        for (int x = 0; x < boardWidth; x++)
+        {
+            for (int y = 0; y < boardHeight; y++)
+            {
+                _board.Cells[x, y] = cloneboard[x, y];
+            }
+        }
     } 
 }
--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
-
 namespace Ucu.Poo.GameOfLife
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BoardReader reader = new BoardReader();
+            bool[,] boardfile = reader.LoadFromFiles("board.txt");
+            
+            Board board = new Board(boardfile);
+            
+            GameLogic game = new GameLogic(board);
+            
+            GamePrinter printer = new GamePrinter();
+
+            while (true)
+            {
+                printer.ShowBoard(board);
+                game.CalculateNextGeneration();
+            }
         }
     }
 }
